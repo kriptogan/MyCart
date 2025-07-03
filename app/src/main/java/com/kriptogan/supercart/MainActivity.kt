66c6b12f9dbe8d5478484948efac387d40220b34
danielProvisionISR <daniel@provisionisr.com>
2025-07-03 15:37:08 +0300

    basic navigation

--- app/src/main/java/com/kriptogan/supercart/MainActivity.kt
+++ app/src/main/java/com/kriptogan/supercart/MainActivity.kt
@@ -4,14 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kriptogan.supercart.ui.theme.SuperCartTheme
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class TabItem(
+    val title: String,
+    val icon: ImageVector
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +39,66 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperCartTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SuperCartApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun SuperCartApp() {
+    var selectedTab by remember { mutableStateOf(0) }
+    
+    val tabs = listOf(
+        TabItem("Home", Icons.Default.Home),
+        TabItem("Shopping List", Icons.Default.ShoppingCart)
     )
+    
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding(), // This ensures content doesn't overlap with status bar
+        bottomBar = {
+            NavigationBar {
+                tabs.forEachIndexed { index, tabItem ->
+                    NavigationBarItem(
+                        selected = selectedTab == index,
+                        onClick = { selectedTab = index },
+                        icon = { Icon(imageVector = tabItem.icon, contentDescription = tabItem.title) },
+                        label = { Text(text = tabItem.title) }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            when (selectedTab) {
+                0 -> HomeScreen()
+                1 -> ShoppingListScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Text(text = "This is Home screen")
+}
+
+@Composable
+fun ShoppingListScreen() {
+    Text(text = "This is Shopping List screen")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SuperCartAppPreview() {
     SuperCartTheme {
-        Greeting("Android")
+        SuperCartApp()
     }
 }
